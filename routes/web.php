dd1109de7dacf9c1ee1e642f2a595359cd608b42
Mahesh <mahi.tirmali.1996@gmail.com>
2023-09-18 21:09:29 +0530

    created routes for login and logout functionality

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 Route::get('/register',[AuthController::class,"loadRegister"])->name("loadRegister");
 Route::post('/register',[AuthController::class,"studentRegister"])->name("studentRegister");
+
+Route::get("/login",function(){
+    return redirect('/');
+});
+
+Route::get('/',[AuthController::class,"loadLogin"])->name("loadLogin");
+Route::post('/',[AuthController::class,"userLogin"])->name("userLogin");
+
+Route::get('/logout',[AuthController::class,"logout"]);
