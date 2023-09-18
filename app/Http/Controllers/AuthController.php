235cf8b9ae391ba426b9c7814bd47c589dd83df0
Mahesh <mahi.tirmali.1996@gmail.com>
2023-09-18 13:55:21 +0530

    Implement Student registration functionality and add server side validation to student registration form

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\support\Facades\Hash;
 
 
 class AuthController extends Controller
 {
     public function loadRegister(){
         return view("register");
+    }
+
+    public function studentRegister(Request $request){
+        $request->validate([
+            "name" => "string|required|min:2",
+            "email" => "string|required|email|max:100|unique:users",
+            "password" => "string|required|confirmed|min:6"
+        ]);
+
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return back()->with("success","Your registration has been successfull.");
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,6 +1,12 @@
 @extends('layout/layout-common')
 
 @section('space-work')
+    <h1>Register</h1>
+    @if ($errors->any())
+       @foreach ($errors->all() as $error)
+           <p style="color: red">{{$error}}</p>
+       @endforeach 
+    @endif
     <form action="{{route('studentRegister')}}" method="post">
         @csrf
         <input type="text" name="name" placeholder="Enter Your Name" />
@@ -12,5 +18,9 @@
         <input type="password" name="password_confirmation" placeholder="Enter confirm Password" />
         <br/><br/>
         <input type="submit" value="Register" />
-    </form>   
+    </form>
+    
+    @if (Session::has('success'))
+    <p style="color: green">{{Session::get('success')}}</p>
+    @endif
 @endsection
